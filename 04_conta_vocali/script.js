afcb6bb120f40c62932fae56d2c567fccf005e38
olegbucolo <olegbucolo97@gmail.com>
2025-12-09 16:00:54 +0100

    feat: finito es. 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa 
+e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function numberOfVocals(str){
+    const vocals = ["a", "e", "i", "o", "u"]
+    let counter = 0;
+    for(let i = 0; i < str.length; i++){
+        if(vocals.includes(str[i])){
+            counter++;
+        }
+    }
+    return counter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numberOfVocals(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
